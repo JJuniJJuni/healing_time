974597517005dd8a77ffa068e428c1a302515c5b
JJuniJJuni <gud305@gmail.com>
2018-10-09 21:46:45 +0900

    add button response for testing

--- inform/views.py
+++ inform/views.py
@@ -1,7 +1,9 @@
 import csv
+import json
+
+from django.http import JsonResponse
 
 from inform.models import Shop
-from django.http import JsonResponse
 
 
 def keyboard(request):
@@ -11,15 +13,34 @@ def keyboard(request):
     })
 
 
+def message(request):
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content']
+    requestMode = return_str.encode('utf-8')  # utf-8형식으로 인코딩하여 한글을 인식
+    if requestMode == '썸톡번역기':
+        return JsonResponse({
+            'message': {
+                'text': Shop.objects.all()[0].title
+            },
+            'keyboard': {
+                'type': 'text'  # 텍스트로 입력받기 위하여 키보드 타입을 text로 설정
+            }
+        })
+
+
 def save_shop_data(path):
     titles = [shop.title.replace(" ", "") for shop in Shop.objects.all()]
     with open(path, mode='r') as csv_file:
         for data in csv.DictReader(csv_file):
             if data["title"].replace(" ", "") not in titles:
-                Shop.objects.create(title=data['title'], link=data['link'], category=data['category'],
-                                  description=data['description'], telephone=data['telephone'],
-                                  address=data['address'], road_address=data['roadAddress'],
-                                  mapX=data['mapx'], mapY=data['mapy'])
+                Shop.objects.create(title=data['title'], link=data['link'],
+                                    category=data['category'],
+                                    description=data['description'],
+                                    telephone=data['telephone'],
+                                    address=data['address'],
+                                    road_address=data['roadAddress'],
+                                    mapX=data['mapx'], mapY=data['mapy'])
 
 
 if __name__ == "__main__":
