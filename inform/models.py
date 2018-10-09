87985822f707087479c93b268f5799f2aa07b28e
JJuniJJuni <gud305@gmail.com>
2018-10-09 20:58:22 +0900

    move function 'read_csv' to view.py

--- inform/models.py
+++ inform/models.py
@@ -1,5 +1,3 @@
-import csv
-
 from django.db import models
 
 
@@ -13,20 +11,3 @@ class Shop(models.Model):
     road_address = models.CharField(max_length=100)
     mapX = models.CharField(max_length=20)
     mapY = models.CharField(max_length=20)
-
-
-def csv_to_model(path):
-    with open(path, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        print(csv_reader)
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                print("Column names are {', '.join(row)}")
-                line_count += 1
-            print(line_count, row)
-            line_count += 1
-
-
-if __name__ == "__main__":
-    csv_to_model("../shop_data/konkuk_shop_info_craw.csv")
--- inform/views.py
+++ inform/views.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.http import JsonResponse
 
 
@@ -6,3 +8,20 @@ def keyboard(request):
         'type': 'buttons',
         'buttons': ['1.healing time', '2.developers'],
     })
+
+
+def read_csv(path):
+    with open(path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        print(csv_reader)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print("Column names are {', '.join(row)}")
+                line_count += 1
+            print(line_count, row)
+            line_count += 1
+
+
+if __name__ == "__main__":
+    read_csv("../shop_data/konkuk_shop_info_craw.csv")
