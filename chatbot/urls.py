a0ddad5d29da5588333cb660f6ff140c99d9acc0
JJuniJJuni <gud305@gmail.com>
2018-10-09 19:26:31 +0900

    add shop data crowled

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.conf.urls import url
 from django.conf.urls import include
+from django.conf.urls import url
+from django.contrib import admin
 
 
 urlpatterns = [
