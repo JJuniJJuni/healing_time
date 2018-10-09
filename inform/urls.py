a64d7a2b07f31d6347e3f1d3691bd876621636ac
JJuniJJuni <gud305@gmail.com>
2018-10-09 22:01:50 +0900

    add url about function 'message'

--- inform/urls.py
+++ inform/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from .views import keyboard
+from .views import message
 
 urlpatterns = [
-    url(r'^keyboard/', keyboard)
+    url(r'^keyboard/', keyboard),
+    url(r'^message', message),
 ]
