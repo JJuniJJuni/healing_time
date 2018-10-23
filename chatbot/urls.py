540d9b8047272c2daa8ba201cf2a96bd26670872
JJuniJJuni <gud305@gmail.com>
2018-10-23 15:31:54 +0900

    remove origin urls

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+
+from inform.views import message
+from inform.views import keyboard
 
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
-    url(r'', include('inform.urls')),
+    url(r'^keyboard/', keyboard),
+    url(r'^message', message),
 ]
--- inform/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .views import keyboard
-from .views import message
-
-urlpatterns = [
-    url(r'^keyboard/', keyboard),
-    url(r'^message', message),
-]
