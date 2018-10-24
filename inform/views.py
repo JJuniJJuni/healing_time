8558d51027954399310547e2157d7337c6223b75
JJuniJJuni <gud305@gmail.com>
2018-10-24 18:42:41 +0900

    add new data

--- inform/views.py
+++ inform/views.py
@@ -34,7 +34,8 @@ def save_shop_data(path):
     with open(path, mode='r') as csv_file:
         for data in csv.DictReader(csv_file):
             if data["title"].replace(" ", "") not in titles:
-                Shop.objects.create(title=data['title'], link=data['link'],
+                Shop.objects.create(place=data['place'], title=data['title'],
+                                    link=data['link'],
                                     category=data['category'],
                                     description=data['description'],
                                     telephone=data['telephone'],
@@ -44,4 +45,4 @@ def save_shop_data(path):
 
 
 if __name__ == "__main__":
-    save_shop_data("../shop_data/konkuk_shop_info_craw.csv")
+    save_shop_data("../shop_data/shop_info_craw_ansi.csv")
