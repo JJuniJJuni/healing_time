efb0a4865789065f131fd00868b87a1149a0a1c9
JJuniJJuni <gud305@gmail.com>
2018-10-29 15:14:03 +0900

    remove url scenario

--- inform/views.py
+++ inform/views.py
@@ -21,22 +21,6 @@ def message(request):
     return_json_str = json.loads(message)
     return_str = return_json_str['content']
     answer = get_answer(return_str)
-    if '주변에 갈만한 곳' in answer:
-        answer = '건대 주변에는 VR 체험, 보드 게임방, 고양이 카페들이 있어요!!'
-    elif '위치' in answer:
-        answer = '하앍'
-        answer = 'https://map.naver.com/?mapmode=0&lng=723' \
-                 'b2d03d073e2849ec2ea83f18361b1&pinId=1951264' \
-                 '1&pinType=site&lat=56c918fb541e28ad8f4efb21ed' \
-                 'a1cf3a&dlevel=11&enc=b64'
-    elif '고양이' in answer:
-        shops = Shop.objects.filter(place='건대', category='테마카페>고양이카페')
-        answer = '건대 주변 고양이 카페로는'
-        for idx, shop in enumerate(shops):
-            answer += '\n{0}.{1}'.format(idx+1, shop.title)
-        answer += '\n위와 같이 있네요!!'
-    elif '데이트 추천' in answer:
-        answer = '데이트로는 단 둘이 보드 게임방 어떠세요?'
     return JsonResponse({
         'message': {
             'text': answer,
