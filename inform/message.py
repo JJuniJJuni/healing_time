4ef42423b4b17c83176d2a21b7d83277c732ba3b
JJuniJJuni <gud305@gmail.com>
2018-10-31 16:46:31 +0900

    print star score

--- inform/message.py
+++ inform/message.py
@@ -20,8 +20,10 @@ def print_shops(question):
     data = question.split()
     place, category = data[0], data[2]
     message = '{} 주변 {} 매장들은 다음과 같이 있어요!!'.format(place, category)
-    shops = [shop for shop in Shop.objects.filter(place=place) if shop.category.endswith(category)]
-    for idx, shop in enumerate(shops):
-        message += '\n{}.{}'.format(idx+1, shop.title)
-    print(message)
+    shops = [shop for shop in Shop.objects.filter(place=place, category__endswith=category)]
+    shops = sorted(shops, key=lambda shop: shop.score, reverse=True)
+    for shop in shops:
+        message += "\n-{}(평점: {}/5)".format(shop.title, shop.score)
+    message += '\n궁금하신 매장 이름을 입력해주세요!!'
+    # print(message)
     return message
