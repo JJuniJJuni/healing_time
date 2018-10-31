87a3d9ee7f27762ec20787c89887373d508c0f66
HyounJun <gud305@gmail.com>
2018-10-31 11:01:28 +0000

    add \n

--- inform/message.py
+++ inform/message.py
@@ -34,11 +34,11 @@ def print_shop_info(question):
     place, title = tokens[0], ' '.join(tokens[1:-1])
     shop = Shop.objects.get(place=place, title=title)
     message = "{place} 지역 '{title}'의 전화번호와 주소는 다음과 같아요!!" \
-              "\n[전화번호]\n{telephone}" \
-              "\n[지도]\n{map_url}" \
-              "\n가격, 이용 시간 등의 정보는 다음 url을 참조하세요!!" \
+              "\n\n[전화번호]\n{telephone}" \
+              "\n\n[지도]\n{map_url}" \
+              "\n\n가격, 이용 시간 등의 정보는 다음 url을 참조하세요!!" \
               "\n{info_url}" \
-              "\n후기 정보가 궁금하시면 '후기'라고 입력해주세요!!".format(place=shop.place,
+              "\n\n후기 정보가 궁금하시면 '후기'라고 입력해주세요!!".format(place=shop.place,
                                                       title=shop.title,
                                                       telephone=shop.telephone,
                                                       map_url=shop.address_url,
