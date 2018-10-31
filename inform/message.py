53add33e3809aec79f7eb0c392ad0b9ed6f0eea0
JJuniJJuni <gud305@gmail.com>
2018-10-31 19:43:29 +0900

    add shop's info url

--- inform/message.py
+++ inform/message.py
@@ -33,13 +33,15 @@ def print_shop_info(question):
     tokens = question.split()
     place, title = tokens[0], ' '.join(tokens[1:-1])
     shop = Shop.objects.get(place=place, title=title)
-    message = "{place} 지역 '{title}'의 전화번호와 주소는 다음과 같아요!!".format(place=shop.place,
-                                                                 title=shop.title)
-    if shop.telephone:
-        message += "\n[전화번호]\n{telephone}".format(telephone=shop.telephone)
-    message += "\n[지도]\n{map_url}" \
-               "\n후기 정보가 궁금하시면 '후기'라고 입력해주세요!!".format(
-                                                       telephone=shop.telephone,
-                                                       map_url=shop.address_url)
+    message = "{place} 지역 '{title}'의 전화번호와 주소는 다음과 같아요!!" \
+              "\n[전화번호]\n{telephone}" \
+              "\n[지도]\n{map_url}" \
+              "\n가격, 이용 시간 등의 정보는 다음 url을 참조하세요!!" \
+              "\n{info_url}" \
+              "\n후기 정보가 궁금하시면 '후기'라고 입력해주세요!!".format(place=shop.place,
+                                                      title=shop.title,
+                                                      telephone=shop.telephone,
+                                                      map_url=shop.address_url,
+                                                      info_url=shop.info_url)
     print(message)
     return message
